42b3101f8d99059ec110480d47b1678ddad575b9
james <jameskmonger@hotmail.co.uk>
2025-12-01 23:24:02 +0000

    create test runner

---
+++ README.md
@@ -0,0 +1,21 @@
+# aoc2025
+
+Trying to learn Rust 🦀
+
+## Usage
+
+```sh
+cargo run -- <day> <part> <optional: test>
+```
+
+e.g. to run day 1, part 1 for the test input
+
+```sh
+cargo run -- 1 1 test
+```
+
+e.g. to run day 6, part 2 for real input
+
+```sh
+cargo run -- 6 2
+```
---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 3 && args.len() != 4 {
+        panic!("Usage: aoc2025 <day> <1 / 2> <optional: test>");
+    }
+
+    let day: u32 = args[1].parse().expect("day must be a number");
+    let part: u32 = args[2].parse().expect("part must be a number");
+
+    let use_test_input = if args.len() == 4 {
+        &args[3] == "test"
+    } else {
+        false
+    };
+
+    let input = get_input(day, use_test_input);
+    let solution_fn = get_solution_fn(day, part);
+    
+    solution_fn(input);
+}
+
+fn get_input(day: u32, use_test_input: bool) -> String {
+    let input_file = if use_test_input { "test" } else { "input" };
+    let input_path = format!("input/{}/{}.txt", day, input_file);
+
+    let input = fs::read_to_string(&input_path)
+        .unwrap_or_else(|_| panic!("Failed to read input file: {}", input_path));
+
+    return input;
+}
+
+fn get_solution_fn(day: u32, part: u32) -> fn(String) {
+    match (day, part) {
+        _ => panic!("Day {} Part {} is not implemented yet", day, part),
+    }
+}
