a3fc7c4d12bedfe5bd166346d8c69f8086c5407e
james <jameskmonger@hotmail.co.uk>
2025-12-01 23:43:29 +0000

    refactor into a single solution

--- src/days/day_01.rs
+++ src/days/day_01.rs
@@ -1,53 +1,4 @@
 pub fn day_01(input: String, part: u8) {
-    if part == 1 {
-        day_1_part_1(input);
-    } else if part == 2 {
-        day_1_part_2(input);
-    } else {
-        panic!("Unrecognised part {}", part);
-    }
-}
-
-fn day_1_part_1(input: String) {
-    let mut dial = 50;
-    let mut zero_hits = 0;
-
-    for line in input.lines() {
-        let direction = &line[0..1];
-        let distance: i32 = line[1..].trim().parse().unwrap();
-
-        if direction == "L" {            
-            dial -= distance;
-        } else if direction == "R" {
-            dial += distance;
-        } else {
-            panic!("unknown direction");
-        }
-
-        while dial < 0 {
-            dial += 100;
-        }
-
-        while dial > 99 {
-            dial -= 100;
-        }
-
-        if dial == 0 {
-            zero_hits += 1;
-        }
-
-        println!("{}{} to {}", direction, distance, dial);
-    }
-
-    println!("");
-    println!("###");
-    println!("");
-
-    // expected test output: 3
-    println!("zero hits: {}", zero_hits);
-}
-
-fn day_1_part_2(input: String) {
     let mut dial = 50;
     let mut zero_hits = 0;
 
@@ -65,16 +16,17 @@ fn day_1_part_2(input: String) {
                 dial -= 100;
             }
 
-            if dial == 0 {
+            // part 2 solution increments counter for any time it passes 0
+            if part == 2 && dial == 0 {
                 zero_hits += 1;
             }
         }
+
+        // part 1 solution only increments the counter if it lands on 0
+        if part == 1 && dial == 0 {
+            zero_hits += 1;
+        }
     }
 
-    println!("");
-    println!("###");
-    println!("");
-
-    // expected test output: 6
     println!("zero hits: {}", zero_hits);
 }
